7054e0dc923a79bd85ded407cc5c60f927bc1d72
Javier <marcin_las@op.pl>
2018-11-03 23:38:33 +0100

    Klasa Diary
    dodanie klasy

---
+++ StudentDiary/Diary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDiary
+{
+    class Diary
+    {
+        // Stan 
+        List<float> ratings;
+
+        // Zachowania
+        public void AddRating(float rating)
+        {
+            ratings.Add(rating);
+        }
+
+        public float CalculateAverage()
+        {
+            float sum = 0; avg = 0;
+
+            foreach (var rating in ratings)
+            {
+                sum = sum + rating;
+            }
+
+            avg = sum / ratings.Count();
+            return avg;
+
+            public float GiveMaxRating()
+            {
+                return ratings.Max();
+            }
+
+            public float GivenMinRating()
+            {
+                return ratings.Min();
+            }
+        }
+    }
+}
