8ecbe834877ca8ccb7fd0089dcf1d15b4bffc3e1
Javier <marcin_las@op.pl>
2018-11-13 19:10:25 +0100

    update

--- StudentDiary/Program.cs
+++ StudentDiary/Program.cs
@@ -23,22 +23,38 @@ namespace StudentDiary
             {
                 Console.WriteLine("Podaj ocenę z zakresu 1 - 10"); 
 
-                float rating = float.Parse(Console.ReadLine());
-
+                float rating;
+                bool result = float.TryParse(Console.ReadLine(), out rating);
 
                 if (rating == 11)
                 {
                     break;
                 }
 
-                diary.AddRating(rating);
+                if (result)
+                {
+                    if (rating > 0 && rating <= 10)
+                    {
+                        diary.AddRating(rating);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Niepoprawna liczba, podaj liczbe z zakresu 1-10");
+                    }
+                    diary.AddRating(rating);
+                }
 
+                diary.AddRating(rating);
 
                 Console.WriteLine("Średnia Twoich ocen to: " + diary.CalculateAverage());
                 Console.WriteLine("Najwyższa ocena to: " + diary.GiveMaxRating());
                 Console.WriteLine("Najniższa ocena to: " + diary.GivenMinRating());
                 Console.ReadKey();
 
+                Diary diary2 = new Diary();
+                Diary diary3 = new Diary();
+                Diary diary4 = new Diary();
+                Diary diary5 = new Diary();
             }
         }
     }
